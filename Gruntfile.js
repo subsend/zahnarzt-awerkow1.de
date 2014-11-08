d5f43dbf6a6f1b010f4a4575be42f4fd830f555b
dawerkow <1q2w3e>
2014-11-08 22:22:36 +0100

    1

---
+++ Gruntfile.js
@@ -0,0 +1,155 @@
+/*
+ * Generated on 2014-10-28
+ * generator-assemble v0.5.0
+ * https://github.com/assemble/generator-assemble
+ *
+ * Copyright (c) 2014 Hariadi Hinta
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+// # Globbing
+// for performance reasons we're only matching one level down:
+// '<%= config.src %>/templates/pages/{,*/}*.hbs'
+// use this if you want to match all subfolders:
+// '<%= config.src %>/templates/pages/**/*.hbs'
+
+module.exports = function(grunt) {
+
+  //load dependencies
+  require('time-grunt')(grunt);
+  require('load-grunt-tasks')(grunt);
+  grunt.loadNpmTasks('assemble');
+
+  // Project configuration.
+  grunt.initConfig({
+
+    config: {
+      src: 'src',
+      dist: 'dist'
+    },
+
+    watch: {
+      assemble: {
+        files: ['<%= config.src %>/{content,data,templates}/{,*/}*.{md,hbs,json}'],
+        tasks: ['assemble']
+      },
+
+      style: {
+        files: ['<%= config.src %>/assets/sass/**/*.scss'],
+        tasks: ['compass']
+        },
+
+      livereload: {
+        options: {
+          livereload: '<%= connect.options.livereload %>'
+        },
+        files: [
+          '<%= config.dist %>/{,*/}*.html',
+          '<%= config.dist %>/assets/css/{,*/}*.css',
+          '<%= config.dist %>/assets/js/{,*/}*.js',
+          '<%= config.dist %>/assets/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+        ]
+      }
+    },
+
+    connect: {
+      options: {
+        port: 9000,
+        livereload: 35729,
+        // change this to '0.0.0.0' to access the server from outside
+        hostname: 'localhost'
+      },
+      livereload: {
+        options: {
+          open: true,
+          base: [
+            '<%= config.dist %>'
+          ]
+        }
+      }
+    },
+
+    assemble: {
+      pages: {
+        options: {
+          flatten: true,
+          assets: '<%= config.dist %>/assets',
+          layout: '<%= config.src %>/templates/layouts/default.hbs',
+          data: '<%= config.src %>/data/*.{json,yml}',
+          partials: '<%= config.src %>/templates/partials/*.hbs',
+          plugins: ['assemble-contrib-sitemap']
+        },
+        files: {
+          '<%= config.dist %>/': ['<%= config.src %>/templates/pages/*.hbs']
+        }
+      }
+    },
+
+    //compass scss
+    compass: {
+      dist: {
+        options: {
+          sassDir: '<%= config.src %>/assets/sass/',
+          cssDir: '<%= config.dist %>/assets/css/',
+          environment: 'production',
+          imagesDir: '<%= config.src %>/assets/images/',
+          fontsDir: '<%= config.src %>/assets/fonts/',
+          noLineComments: true,
+
+        }
+      }
+    },
+
+    //imagemin
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: '<%= config.src %>/assets/images/',
+          src: ['**/*.{png,jpg,gif,svg}'],
+          dest: '<%= config.dist %>/assets/images/'
+        }]
+      }
+    },
+
+    //concat
+    concat: {
+      options: {
+        separator: ';'
+      },
+      js: {
+        src: ['<%= config.src %>/assets/js/*.js'],
+        dest: '<%= config.dist %>/assets/js/main.js'
+      },
+      css: {
+        src: ['<%= config.src %>/assets/css/*.css'],
+        dest: '<%= config.dist %>/assets/css/main.css'
+      }
+    },
+
+    // Before generating any new files,
+    // remove any previously-created files.
+    clean: ['<%= config.dist %>/**/*.{html,xml,css,js}']
+
+  });
+
+  grunt.registerTask('server', [
+    'build',
+    'connect:livereload',
+    'watch'
+  ]);
+
+  grunt.registerTask('build', [
+    'clean',
+    'assemble',
+    'imagemin',
+    'concat'
+  ]);
+
+  grunt.registerTask('default', [
+    'build'
+  ]);
+
+};
